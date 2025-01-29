6bb8365475d3fb6943dfae5fd0b5eba5b94898de
Diogo Santos <diogoejsantos@ua.pt>
2025-01-29 17:18:36 +0000

    Refactored code

--- src/client_main.c
+++ src/client_main.c
@@ -2,40 +2,53 @@
 #include "game_state.h"
 #include <stdio.h>
 
+static int get_column_input(void) {
+    int column;
+    printf("Your turn! Enter column (1-%d): ", BOARD_COLS);
+    if (scanf("%d", &column) != 1) {
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
+    return column - 1;
+}
+
+static int send_move(int column) {
+    char column_message = (char) (column + '0');
+    return client_socket_send(&column_message, sizeof(char));
+}
+
+static GameState* receive_game_state(void) {
+    char message[BUFFER_SIZE] = {0};
+    int bytes_received = client_socket_read(message);
+    if (bytes_received < 0) {
+        return NULL;
+    }
+    return game_state_deserialize(message, BUFFER_SIZE);
+}
+
 int main(void) {
     printf("Starting client...\n");
     client_socket_init();
-    char message[BUFFER_SIZE] = {0};
-    client_socket_read(message);
-    GameState* game_state = game_state_deserialize(message, BUFFER_SIZE);
-    game_state_print(game_state);
+    GameState* game_state = receive_game_state();
     int player_index = game_state->current_player_index == 0;
+    game_state_print(game_state);
     printf("You play as %c!\n", player_index ? PLAYER1 : PLAYER2);
-    int bytes_sent, bytes_received, column;
-    char column_message;
+    int column;
     while (!game_state->game_over) {
-        printf("Your turn! Enter column (1-%d): ", BOARD_COLS);
-        if (scanf("%d", &column) != 1) {
-            while (getchar() != '\n');
+        column = get_column_input();
+        if (column < 0 || column >= BOARD_COLS) {
+            printf("Invalid column (%d). Try again.\n", column + 1);
             continue;
         }
-        while (getchar() != '\n');
-        if (column < 1 || column > BOARD_COLS) {
-            printf("Invalid column (%d). Try again.\n", column);
-            continue;
-        }
-        column_message = (char) (column - 1 + '0');
-        bytes_sent = client_socket_send(&column_message, sizeof(char));
-        if (bytes_sent < 0) {
-            perror("Internal error. Terminating...\n");
+        if (send_move(column) < 0) {
+            fprintf(stderr, "Internal error. Terminating...\n");
             break;
         }
-        bytes_received = client_socket_read(message);
-        if (bytes_received < 0) {
-            perror("Internal error. Terminating...\n");
+        game_state = receive_game_state();
+        if (game_state == NULL) {
+            fprintf(stderr, "Internal error. Terminating...\n");
             break;
         }
-        game_state = game_state_deserialize(message, BUFFER_SIZE);
         game_state_print(game_state);
     } 
     if (game_state->current_player_index != player_index) {
--- src/server_main.c
+++ src/server_main.c
@@ -6,51 +6,79 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define SWITCH_PLAYER(game_state) ((game_state)->current_player_index = ((game_state)->current_player_index + 1) % MAX_PLAYERS)
+
+static int handle_client_connection(GameState* game_state) {
+    static int player_index = 0;
+    int client_fd = server_socket_accept();
+    if (client_fd < 0) {
+        return -1;
+    }
+    game_state->players[player_index].socket_fd = client_fd;
+    game_state->players[player_index].marker = (player_index == 0) ? PLAYER1 : PLAYER2;
+    printf("Client %d connected\n", player_index + 1);
+    player_index++;
+    return client_fd;
+}
+
+static int send_game_state(GameState* game_state) {
+    char message[BUFFER_SIZE] = {0};
+    int bytes_written = game_state_serialize(game_state, message, BUFFER_SIZE);
+    return server_socket_send(game_state->players[game_state->current_player_index].socket_fd, message, bytes_written);
+}
+
+static int get_move(GameState* game_state) {
+    char message[BUFFER_SIZE] = {0};
+    int bytes_received = server_socket_read(game_state->players[game_state->current_player_index].socket_fd, message);
+    if (bytes_received < 0) {
+        return -1;
+    }
+    return message[0] - '0';
+}
+
+static void announce_game_winner(GameState* game_state) {
+    char message[BUFFER_SIZE] = {0};
+    int bytes_written = game_state_serialize(game_state, message, BUFFER_SIZE);
+    server_socket_send(game_state->players[0].socket_fd, message, bytes_written);
+    server_socket_send(game_state->players[1].socket_fd, message, bytes_written);
+}
+
 int main() {
     GameState* game_state = game_state_init();
     if (game_state == NULL) {
         fprintf(stderr, "Failed to initialize game state.\n");
         return 1;
     }
-
     server_socket_init();
     printf("Server waiting for connections...\n");
 
-    int client_fd1 = server_socket_accept();
+    int client_fd1 = handle_client_connection(game_state);
     if (client_fd1 < 0) {
         fprintf(stderr, "Failed to accept client 1.\n");
         game_state_close(&game_state);
+        server_socket_close();
         return 1;
     }
-    game_state->players[0].socket_fd = client_fd1;
-    game_state->players[0].marker = PLAYER1;
-    printf("Client 1 connected\n");
 
-    int client_fd2 = server_socket_accept();
+    int client_fd2 = handle_client_connection(game_state);
     if (client_fd2 < 0) {
         fprintf(stderr, "Failed to accept client 2.\n");
         game_state_close(&game_state);
+        server_socket_close();
         return 2;
     }
-    game_state->players[1].socket_fd = client_fd2;
-    game_state->players[1].marker = PLAYER2;
-    printf("Client 2 connected\n");
 
-    char message[BUFFER_SIZE] = {0};
-    int bytes_written, bytes_sent, bytes_received, column;
+    int column;
     while (!game_state->game_over) {
-        bytes_written = game_state_serialize(game_state, message, BUFFER_SIZE);
-        bytes_sent = server_socket_send(game_state->players[game_state->current_player_index].socket_fd, message, bytes_written);
-        if (bytes_sent < 0) {
-            perror("Client disconnected!\n");
+        if (send_game_state(game_state) < 0) {
+            fprintf(stderr, "Client disconnected!\n");
             break;
         }
-        bytes_received = server_socket_read(game_state->players[game_state->current_player_index].socket_fd, message);
-        if (bytes_received < 0) {
-            perror("Internal error. Terminating...\n");
+        column = get_move(game_state);
+        if (column == -1) {
+            fprintf(stderr, "Internal error. Terminating...\n");
             break;
         }
-        column = message[0] - '0';
         if (!game_state_make_move(game_state, column)) continue;
         printf("Client %d's turn:\n", game_state->current_player_index + 1);
         game_state_print(game_state);
@@ -59,11 +87,10 @@ int main() {
             printf("Client %d wins!\n", game_state->current_player_index + 1);
             break;
         }
-        game_state->current_player_index = (game_state->current_player_index + 1) % MAX_PLAYERS;
+        SWITCH_PLAYER(game_state);
     }
-    bytes_written = game_state_serialize(game_state, message, BUFFER_SIZE);
-    server_socket_send(client_fd1, message, bytes_written);
-    server_socket_send(client_fd2, message, bytes_written);
+
+    announce_game_winner(game_state);
 
     server_socket_close_client_socket(client_fd1);
     server_socket_close_client_socket(client_fd2);
